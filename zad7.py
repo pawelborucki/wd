b71941fa4df7c212e74172811dc5bbd98ed060cb
pawelborucki <pawel.borucki@student.uwm.edu.pl>
2021-03-10 08:13:04 +0100

    Praca domowa 3

--- zad7.py
+++ zad7.py
@@ -1,6 +1,10 @@
 imie = "Paweł"
 nazwisko = "Borucki"
-tekst = " w tekscie jest "
-litera_1 = imie[2]
-litera_2 = nazwisko[3]
-print(f"w tekscie jest {} liter w oraz {} liter t")
+tekst = """ Lorem Ipsum jest tekstem stosowanym jako przykładowy wypełniacz w przemyśle poligraficznym.
+Został po raz pierwszy użyty w XV w. przez nieznanego drukarza do wypełnienia tekstem próbnej książki.
+Pięć wieków później zaczął być używany przemyśle elektronicznym, pozostając praktycznie niezmienionym. 
+Spopularyzował się w latach 60. XX w. wraz z publikacją arkuszy Letrasetu, zawierających fragmenty Lorem Ipsum, 
+a ostatnio z zawierającym różne wersje Lorem Ipsum oprogramowaniem przeznaczonym do realizacji druków na komputerach osobistych, jak Aldus PageMaker """
+litera_1 = tekst.count(imie[2])
+litera_2 = tekst.count(nazwisko[3])
+print(f"w tekscie jest {litera_1} liter w oraz {litera_2} liter u")
