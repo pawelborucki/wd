c932b35ff85eecfb916d03d2ea69f19541f36ebc
pawelborucki <pawel.borucki@student.uwm.edu.pl>
2021-03-16 18:58:01 +0100

    pd lab3

--- lab3/zad2.py
+++ lab3/zad2.py
@@ -1,4 +1,4 @@
 import random
 macierz = [[random.random() for j in range(4)] for i in range(4)]
-przekatna = [[x for x in macierz] for i in macierz]
+przekatna = [macierz[x][x] for x in range(4)]
 print(przekatna)
