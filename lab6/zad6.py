f4878bcc7727dc639f2969cddfc4c75d6621e471
pawelborucki <pawel.borucki@student.uwm.edu.pl>
2021-04-27 16:44:22 +0200

    lab6

--- lab6/zad6.py
+++ lab6/zad6.py
@@ -1,7 +1,9 @@
 import numpy as np
 
-w1 = "TAK"
-w2 = "KAT"
-w3 = "TAT"
-mat = np.array([list(w1), list(w2)])
-print(mat)
+w1 = "mama"
+w2 = "mata"
+w3 = "mózg"
+mat1 = np.diag(list(w1))
+mat1[0:4,0] = list(w3)
+mat1[0,0:4] = list(w2)
+print(mat1)
