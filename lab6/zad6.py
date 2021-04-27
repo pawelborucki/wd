c486f2366353833d9c3b9f3dd63e18a2c392e823
pawelborucki <pawel.borucki@student.uwm.edu.pl>
2021-04-27 16:58:02 +0200

    lab6

--- lab6/zad6.py
+++ lab6/zad6.py
@@ -1,9 +1,10 @@
 import numpy as np
-
-w1 = "mama"
+import random
+import string
+w1 = "szum"
 w2 = "mata"
 w3 = "mózg"
-mat1 = np.diag(list(w1))
+mat1 = np.diag(list(w1)[::-1])
 mat1[0:4,0] = list(w3)
 mat1[0,0:4] = list(w2)
 print(mat1)
